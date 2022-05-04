b1dd009ea28a5cb436009e12938356420ce424d1
JoseMG92 <ppmezag@gmail.com>
2022-05-04 13:06:47 -0500

    primer commit

---
+++ todohubmine/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
