4802219a72d86052e13c56943bb2c7300c69251d
Hever <60167940+h-rub@users.noreply.github.com>
2022-05-05 12:23:15 -0500

    Delete unused view

--- todohubmine/api/urls.py
+++ todohubmine/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import UsuarioView, TaskView
+from .views import UsuarioView
 
 urlpatterns = [
     #path('usuarios/registro',UsuarioView.as_view(), name='usuarios_list')
     path('auth/register',UsuarioView.as_view(), name='usuarios_list'),
-    path('task/register',TaskView.as_view(), name='task_list')
 ]
